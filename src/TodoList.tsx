8cf56ee9ed0385cfb65029b3b4d5f6dbdeed1267
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-04-03 00:18:34 +0300

    lesson-1 refactoring

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,26 +1,20 @@
-import React from "react";
-
 type TodoListPropsType = {
     title: string
     tasks: Array<TaskType>
 }
 
-type TaskType = {
+export type TaskType = {
     id: number
     title: string
     isDone: boolean
 }
 
 export const TodoList = ({title, tasks}: TodoListPropsType) => {
-   /* const title = props.title
-    const tasks = props.tasks*/
-
-    //const {title, tasks} = props
 
     const tasksList: Array<JSX.Element> = tasks.map(task => {
         return (
             <li>
-                <input type= "checkbox" checked={task.isDone} />
+                <input type="checkbox" checked={task.isDone}/>
                 <span>{task.title}</span>
             </li>
         )
