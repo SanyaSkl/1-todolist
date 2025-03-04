23aacb3f349153a9cdd812edef710cb61139124c
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2025-03-05 01:46:08 +0300

    lesson todoList 14.1: app

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import { Provider } from "react-redux"
 import { AppHttpRequests } from "app/AppHttpRequests"
 import { store } from "app/store"
 import "./index.css"
+import App from "app/App"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/*<App />*/}
-      <AppHttpRequests />
+      <App />
+      {/*<AppHttpRequests />*/}
     </Provider>
   </React.StrictMode>,
 )
